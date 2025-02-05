f65648fd5f23549b14105035836978f256d1b954
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2025-02-05 20:50:52 +0000

    -

--- Projects/PS Admin Function Dashboard/PS Admin Dashboard.ps1
+++ Projects/PS Admin Function Dashboard/PS Admin Dashboard.ps1
@@ -79,7 +79,7 @@ $PasswordGeneratorButton.Add_Click({
 $NetworkGetIPEtcButton = New-Object System.Windows.Forms.Button
 $NetworkGetIPEtcButton.Text = "Network Configuration"
 $NetworkGetIPEtcButton.Size = New-Object System.Drawing.Size(150, 40)
-$NetworkGetIPEtcButton.Location = New-Object System.Drawing.Point(200, 40)
+$NetworkGetIPEtcButton.Location = New-Object System.Drawing.Point(200, 50)
 $NetworkGetIPEtcButton.Add_Click({
     
     $NetConf = Get-NetIPConfiguration | Select-Object InterfaceAlias, IPv4Address, IPv6Address, DNSServer, InterfaceDescription, @{Name="Gateway";Expression={$_.IPv4DefaultGateway}}, @{Name="MACAddress";Expression={(Get-NetAdapter -InterfaceIndex $_.InterfaceIndex).MacAddress}}
