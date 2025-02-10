cd01116772cd32b77111dd2be6597acbdcd8ef09
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2025-02-10 17:28:09 +0000

    -

--- Useful IT Admin Scripts/PS Get Network Configuration.ps1
+++ Useful IT Admin Scripts/PS Get Network Configuration.ps1
@@ -1,4 +1,4 @@
 $NetConf = Get-NetIPConfiguration | Select-Object InterfaceAlias, IPv4Address, IPv6Address, DNSServer, InterfaceDescription, @{Name="Gateway";Expression={$_.IPv4DefaultGateway}}, @{Name="MACAddress";Expression={(Get-NetAdapter -InterfaceIndex $_.InterfaceIndex).MacAddress}}
 
-Write-Output "Current Computer Network Configuration: `n" 
+Write-Output "Current Computer Network Configuration: `n"  
 $NetConf
--- Useful IT Admin Scripts/Restart DNS.ps1
+++ Useful IT Admin Scripts/Restart DNS.ps1
@@ -1,3 +1,4 @@
+#Restart DNS service and clear cache
 Restart-Service -Name Dnscache -Force
 Clear-DnsClientCache
 Write-Host "DNS Service restarted and cache flushed successfully." -ForegroundColor Green
